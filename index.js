634e3679e2079f4b875bad1c9285a0f8135b2e36
Prashant Patil <71242854+PrashantPatil10178@users.noreply.github.com>
2024-04-01 22:02:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,18 @@ const Friend = require("./db/friends.model");
 dotenv.config({ path: "./.env" });
 
 (async function () {
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({
+    args: [
+      "--disable-setuid-sandbox",
+      "--no-sandbox",
+      "--single-process",
+      "--no-zygote",
+    ],
+    executablePath:
+      process.env.NODE_ENV === "production"
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),
+  });
   const page = await browser.newPage();
   await page.goto("https://ioit.akronsystems.com/pLogin.aspx");
 
